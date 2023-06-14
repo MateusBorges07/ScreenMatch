c051fd2556f4282ba695911d6fd74599dcc66aba
MateusBorges07 <mateusinfo36@gmail.com>
2023-06-13 21:52:07 -0300

    Correção

--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -16,14 +16,4 @@ public class CalculadoraDeTempo {
         this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
 
-//    public void inclui(Filme f){
-//        this.tempoTotal += f.getDuracaoEmMinutos();
-//
-//
-//    }
-//    public void inclui(Serie s){
-//        this.tempoTotal += s.getDuracaoEmMinutos();
-//
-//
-//    }
 }
